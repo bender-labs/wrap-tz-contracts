daf41a8640c2001330f3a41869063cec60866cf3
Jean-Baptiste Dusseaut <jb@benderlabs.io>
2021-01-20 17:40:33 +0100

    fix: proper use of cid in quorum helper

--- src/quorum.py
+++ src/quorum.py
@@ -8,13 +8,12 @@ class Quorum(object):
 
     def mint(self, contract_id, minter_contract, owner, amount, tx_id, token_id, signer_id, signature):
         contract = self.client.contract(contract_id)
-        id = cid.from_string(signer_id)
         mint = {"amount": amount, "owner": owner,
                 "token_id": token_id,
                 "tx_id": tx_id}
         print(contract.minter)
         op = contract \
-            .minter(signatures=[[id.multihash, signature]],
+            .minter(signatures=[[signer_id, signature]],
                     action={"target": f"{minter_contract}%signer",
                             "entry_point": {"mint_token": mint}},
                     ) \
