ae8c9c44a89f5779b5cc5716f4afa5dd681233f2
Jean-Baptiste Dusseaut <jb@benderlabs.io>
2021-03-23 18:33:25 +0100

    feat: generic multisig governance: distribute wrap token supply

--- admin/governance/governance.mk
+++ admin/governance/governance.mk
@@ -20,3 +20,27 @@ $(OUT)/governance_confirm_bender_migration.tz: governance/governance_confirm_ben
 	$(COMPILE_PARAMETER) '((counter, Operation confirm_migration), signatures)'
 
 governance_confirm_bender_migration_call: $(OUT)/governance_confirm_bender_migration.tz
+
+#--- DISTRIBUTE GOVERNANCE TOKEN
+
+$(OUT)/governance_distribute.mligo: target_address = $(DEFAULT_CONTRACT_TARGET)
+$(OUT)/governance_distribute.mligo: counter = 0
+$(OUT)/governance_distribute.mligo: $(OUT)/common_vars.mligo
+	$(file >$@,let counter = $(counter)n)
+	$(file >>$@,let contract_address = ("$(target_address)":address))
+	$(file >>$@,let distribution:distribute_param = [])
+	$(file >>$@,let signatures: signature option list = [])
+
+governance_distribute_params: $(OUT)/governance_distribute.mligo
+
+$(OUT)/governance_distribute.payload: governance/governance_distribute.mligo $(OUT)/governance_distribute.mligo
+	$(eval PAYLOAD := $(shell $(COMPILE_EXPRESSION) $(notdir $(basename $@))_payload))
+	$(file >$@,$(PAYLOAD))
+
+governance_distribute_payload: $(OUT)/governance_distribute.payload
+
+
+$(OUT)/governance_distribute.tz: governance/governance_distribute.mligo $(OUT)/governance_distribute.mligo
+	$(COMPILE_PARAMETER) '((counter, Operation distribute), signatures)'
+
+governance_distribute_call: $(OUT)/governance_distribute.tz
